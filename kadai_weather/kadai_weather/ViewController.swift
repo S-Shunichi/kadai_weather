bfdcf3a97bf813b4bb2c9ecf2a5ddf3f0cf4a3cc
S-Shunichi <shunichisakaguchi@icloud.com>
2015-05-17 20:20:00 +0900

    UIPickerViewのdelegateとdataSourceへ、selfの設定ができない。エラーが出る。

--- kadai_weather/kadai_weather/ViewController.swift
+++ kadai_weather/kadai_weather/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var m_LocationPicker : UIPickerView!
+    private let m_PickerValue : NSArray = ["東京", "大阪", "福岡"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // プルダウンの設置
+        m_LocationPicker = UIPickerView()
+        m_LocationPicker.frame = CGRectMake(0, 0, self.view.bounds.width, 180)
+        m_LocationPicker.delegate = self
+        m_LocationPicker.dataSource = self
+        self.view.addSubview(m_LocationPicker)
     }
 
+    /*
+    pickerに表示する列数を返すデータソースメソッド.
+    (実装必須)
+    */
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    /*
+    pickerに表示する行数を返すデータソースメソッド.
+    (実装必須)
+    */
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return m_PickerValue.count
+    }
+    
+    /*
+    pickerに表示する値を返すデリゲートメソッド.
+    */
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String {
+        return m_PickerValue[row] as! String
+    }
+    
+    /*
+    pickerが選択された際に呼ばれるデリゲートメソッド.
+    */
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        println("row: \(row)")
+        println("value: \(m_PickerValue[row])")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
