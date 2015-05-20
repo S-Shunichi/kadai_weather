04ef2a0ecdcd50fa787b6b2fee84fb2d5c9e67a9
S-Shunichi <shunichisakaguchi@icloud.com>
2015-05-20 09:37:41 +0900

    JSONデータの取得失敗は、ウイルスバスターで引っかかっているため。 画像のサイズの取得が上手くいっていないが、一応課題完了。

--- kadai_weather/kadai_weather/ViewController.swift
+++ kadai_weather/kadai_weather/ViewController.swift
@@ -29,6 +29,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         m_LocationPicker.delegate = self
         m_LocationPicker.dataSource = self
         self.view.addSubview(m_LocationPicker)
+        
+        // アイコン初期表示
+        DisplayWeatherIcon(0)
     }
 
     /*
@@ -61,6 +64,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         println("row: \(row)")
         println("value: \(m_PickerValue[row])")
         
+        DisplayWeatherIcon(row)
+    }
+    
+    func DisplayWeatherIcon( row:Int ){
         // JSONで天気データを取得
         var json_url : NSURL = NSURL(string: "")!
         switch row{
@@ -70,18 +77,41 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         case 1: // osaka
             json_url = OSAKA_URL!
             break
-        //case 2:
+            //case 2:
         default: // fukuoka
             json_url = FUKUOKA_URL!
             break
             
         }
+        
         println(json_url)
         var json_data = NSData(contentsOfURL: json_url)
         let json = JSON(data:json_data!)
         
-        var tenki_string:String = json["forecasts"]["telop"].string!
-        println(tenki_string)
+        // 画像の取得
+        // 今日
+        var tenki_today_string:String = json["forecasts"][0]["telop"].string!
+        println(tenki_today_string)
+        
+        //var imagesize_width = (json["forecasts"][0]["image"]["width"].string!).toInt()
+        //var imagesize_height = (json["forecasts"][0]["image"]["height"].string!).toInt()
+        var imagesize_width = Int(50)
+        var imagesize_height = Int(31)
+        var pos_x = self.view.bounds.width/2 - CGFloat(imagesize_width/2)
+        
+        var image_url = NSURL(string:json["forecasts"][0]["image"]["url"].string!)
+        var image_data = NSData(contentsOfURL:image_url!)
+        var image = UIImage(data: image_data!)
+        var image_view = UIImageView(image: image)
+        image_view.tag = 1
+        image_view.frame = CGRectMake(pos_x, CGFloat(300), CGFloat(imagesize_width), CGFloat(imagesize_height))
+        
+        for subview in self.view.subviews{
+            if (1 == subview.tag){
+                subview.removeFromSuperview()
+            }
+        }
+        self.view.addSubview(image_view)
     }
     
     override func didReceiveMemoryWarning() {
